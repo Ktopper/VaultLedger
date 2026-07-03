9f8105d4df8ea6bd40214ac1995860e253a56d04
Ktopper <fervorbooks@gmail.com>
2026-07-02 20:43:32 -0400

    feat(core): export schemas and zones

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,5 @@
 export const VERSION = "0.1.0";
+export * from "./schemas/provenance.js";
+export * from "./schemas/operation.js";
+export * from "./schemas/manifest.js";
+export * from "./zones.js";
